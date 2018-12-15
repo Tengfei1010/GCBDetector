c8cf794dcf89dcecb3d416e20d60a0bd7b78a79c
tengfei <tutengfei.kevin@gmail.com>
2018-12-15 19:04:13 +0800

    fix a typo

--- callgraph/rta/rta.go
+++ callgraph/rta/rta.go
@@ -40,7 +40,7 @@
 // cmd/callgraph tool on its own source takes ~2.1s for RTA and ~5.4s
 // for points-to analysis.
 //
-package rta // import "honnef.co/go/tools/callgraph/rta"
+package rta
 
 // TODO(adonovan): test it by connecting it to the interpreter and
 // replacing all "unreachable" functions by a special intrinsic, and
